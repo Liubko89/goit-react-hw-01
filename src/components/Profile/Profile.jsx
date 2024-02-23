50acc36813f97001048690ca956909d416798ece
Liubko89 <142455025+Liubko89@users.noreply.github.com>
2024-02-23 00:44:03 +0000

    profile markup

---
+++ src/components/Profile/Profile.jsx
@@ -0,0 +1,34 @@
+function Profile({
+  name,
+  tag,
+  location,
+  image,
+  stats: { followers, views, likes },
+}) {
+  return (
+    <div>
+      <div>
+        <img src={image} alt={tag} width={300} />
+        <p>{name}</p>
+        <p>{tag}</p>
+        <p>{location}</p>
+      </div>
+      <ul>
+        <li>
+          <span>Followers</span>
+          <span>{followers}</span>
+        </li>
+        <li>
+          <span>Views</span>
+          <span>{views}</span>
+        </li>
+        <li>
+          <span>Likes</span>
+          <span>{likes}</span>
+        </li>
+      </ul>
+    </div>
+  );
+}
+
+export default Profile;
